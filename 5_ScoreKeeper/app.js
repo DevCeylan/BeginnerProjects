d1bd792aee462f86ac3700b8062e2b6240883f49
Mehmet CEYLAN <ceylarino@gmail.com>
2021-12-10 16:21:02 +0300

    score adding + gameover logic

--- 5_ScoreKeeper/app.js
+++ 5_ScoreKeeper/app.js
@@ -2,8 +2,30 @@
 const p1button = document.querySelector("#btnPlayer1");
 const p2button = document.querySelector("#btnPlayer2");
 
+const p1display = document.querySelector("#player1-score");
+const p2display = document.querySelector("#player2-score");
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let isGameover = false;
 // Event Listeners
 
 p1button.addEventListener("click", function () {
-  alert("Clicked");
+  if (!isGameover) {
+    p1Score += 1;
+    if (p1Score === winningScore) {
+      isGameover = true;
+    }
+    p1display.textContent = p1Score;
+  }
+});
+p2button.addEventListener("click", function () {
+  if (!isGameover) {
+    p2Score += 1;
+    if (p2Score === winningScore) {
+      isGameover = true;
+    }
+    p2display.textContent = p2Score;
+  }
 });
