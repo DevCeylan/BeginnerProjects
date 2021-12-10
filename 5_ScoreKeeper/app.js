9e486b168c7a59ce19b1aed4c29c876cf8549ef5
Mehmet CEYLAN <ceylarino@gmail.com>
2021-12-10 16:06:33 +0300

    initial files + eventListeners test

---
+++ 5_ScoreKeeper/app.js
@@ -0,0 +1,9 @@
+// DOM elements:
+const p1button = document.querySelector("#btnPlayer1");
+const p2button = document.querySelector("#btnPlayer2");
+
+// Event Listeners
+
+p1button.addEventListener("click", function () {
+  alert("Clicked");
+});
