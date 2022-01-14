6cb96ec0b969f9fdc53bb61bc99fa5816c8128c2
Mehmet CEYLAN <ceylarino@gmail.com>
2022-01-14 15:36:20 +0300

    SearchApp

---
+++ 10_TvShowSearchApp/app.js
@@ -0,0 +1,22 @@
+// DOM objects
+const form = document.querySelector("#searchForm");
+
+// Eventlisteners
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  setImage(res.data);
+  form.elements.query.value = "";
+});
+
+const setImage = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
